282217366b540902531c1d2f323a43c01d3ba0ef
gofeng117 <gofeng117@163.com>
2015-05-21 22:32:05 +0800

    done the JWT demo

--- angular-jwt-authentication/public/app.js
+++ angular-jwt-authentication/public/app.js
@@ -1,6 +1,8 @@
 (function(){
     'use strict';
-    var app = angular.module('app',[]);
+    var app = angular.module('app' ,[] , function config($httpProvider){
+        $httpProvider.interceptors.push('AuthInterceptor');
+    });
 
     app.constant('API_URL','http://localhost:3000');
 
@@ -8,6 +10,14 @@
         'use strict';
         $scope.getRandomUser = getRandomUser;
         $scope.login = login;
+        $scope.logout = logout;
+
+        // initialization
+
+        UserFactory.getUser().then(function success(response){
+            $scope.user = response.data;
+        })
+
         function getRandomUser(){
             RandomUserFactory.getUser().then(function success(response){
                 $scope.randomUser = response.data;
@@ -16,9 +26,14 @@
 
         function login(username,password){
             UserFactory.login(username,password).then(function success(response){
-                $scope.user = response.data;
-                alert(response.data.token);
+                $scope.user = response.data.user;
+                //alert(response.data.token);
             },handleError);
+        }
+
+        function logout(){
+            UserFactory.logout();
+            $scope.user = null;
         }
 
         function handleError(response){
@@ -38,18 +53,74 @@
         }
     });
 
-    app.factory('UserFactory',function UserFactory($http,API_URL){
+    app.factory('UserFactory',function UserFactory($http,$q,API_URL,AuthTokenFactory){
         'use strict';
         return {
-            login : login
+            login : login,
+            logout : logout,
+            getUser : getUser
         };
 
         function login(username,password){
             return $http.post(API_URL + '/login',{
                 username : username,
                 password : password
+            }).then(function success(respose){
+                AuthTokenFactory.setToken(respose.data.token);
+                return respose;
             });
+        }
+
+        function logout(){
+            AuthTokenFactory.setToken();
+        }
+
+        function getUser(){
+            if(AuthTokenFactory.getToken()){
+                return $http.get(API_URL + '/me');
+            }else{
+                return $q.reject({data : 'client has no auth token'});
+            }
         }
     });
 
+    app.factory('AuthTokenFactory',function AuthTokenFactory($window){
+        'use strict';
+        var store = $window.localStorage;
+        var key = 'auth-token';
+
+        return {
+            getToken : getToken,
+            setToken : setToken
+        }
+
+        function getToken(){
+            return store.getItem(key);
+        }
+
+        function setToken(token){
+            if(token){
+                store.setItem(key,token);
+            }else{
+                store.removeItem(key);
+            }
+        }
+    });
+
+    app.factory('AuthInterceptor',function AuthInterceptor(AuthTokenFactory){
+        'use strict';
+        return {
+            request : addToken
+        };
+
+        function addToken(config){
+            var token = AuthTokenFactory.getToken();
+            if(token){
+                config.headers = config.headers || {};
+                config.headers.Authorization = 'Bearer ' + token;
+                //config.headers.shenTest = 'hi';
+            }
+            return config;
+        }
+    });
 })();
--- angular-jwt-authentication/server.js
+++ angular-jwt-authentication/server.js
@@ -3,47 +3,72 @@ var faker = require('faker');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 var jwtSecret = 'akhdklajsdqwl561/sdaxcf';
 
-var user = {
-    username : 'shenxiaolei',
-    password : 'hi'
-}
+var users = [
+    {
+        username: 'shenxiaolei',
+        password: 'hi'
+    },
+    {
+        username: 'liwei',
+        password: 'oh'
+    },
+]
+
+var currentuser = {};
 
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(expressJwt({secret: jwtSecret}).unless({path: ['/login']}));
 
-app.get('/random-user',function(req,res){
+app.get('/random-user', function (req, res) {
     var user = faker.helpers.userCard();
     user.avatar = faker.image.avatar();
     res.json(user);
 });
 
-app.post('/login',authenticate,function(req,res){
+app.get('/me', function (req, res) {
+    res.send(req.user);
+})
+
+app.post('/login', authenticate, function (req, res) {
     var token = jwt.sign({
-        username : user.username
-    },jwtSecret);
+        username: currentuser.username
+    }, jwtSecret);
     res.send({
-        token : token,
-        user : user
+        token: token,
+        user: currentuser
     });
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('App Listening on localhost:3000');
 })
 
-// ¸¨Öú·½·¨
-function authenticate(req,res,next){
+// authenticate
+function authenticate(req, res, next) {
     var body = req.body;
-    if(!body.username || !body.password){
+    var login = false;
+    if (!body.username || !body.password) {
         res.status(400).end('must provide username or password');
     }
 
-    if(body.username !== user.username || body.password !== user.password){
+    for(var i = 0; i < users.length;i++){
+        if (body.username === users[i].username && body.password === users[i].password) {
+            currentuser = users[i];
+            login = true;
+        }
+    }
+
+    if(!login){
         res.status(401).end('username or password incorrect');
+
     }
     next();
+
+
 }
